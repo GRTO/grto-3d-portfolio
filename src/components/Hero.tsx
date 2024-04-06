3c6b288da4879b3776a70e0b21efc169356f34e6
Gerson <gerson.toribio@pucp.pe>
2024-04-07 00:24:25 +0100

    Add portfolio description

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,28 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-  return <section className="relative w-full h-screen mx-auto">Hero</section>;
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-90 bg-gradient-to-b from-violet-500" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I am <span className="text-[#915eff]">Gerson</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D solutions, user <br className="sm:block hidden" />
+            interfaces, web applications with React, Typescript and ThreeJs
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
 };
 
 export default Hero;

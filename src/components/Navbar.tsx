3f9b770f7a1865ce381a0e66aa37c66e73726bda
Gerson <gerson.toribio@pucp.pe>
2024-04-06 18:55:41 +0100

    Update navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
@@ -22,8 +22,8 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text=[18px] font-bold cursor-pointer">
-            Gerson <span className="sm:block hidden"> | Typescript</span>
+          <p className="text-white text=[18px] font-bold cursor-pointer flex">
+            Gerson &nbsp; <span className="sm:block hidden"> | Typescript</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -52,14 +52,17 @@ const Navbar = () => {
             !toggle ? "hidden" : "flex"
           } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}
         >
-          <ul className="list-none hidden sm:flex flex-row gap-10">
+          <ul className="list-none flex justify-end items-start flex-col gap-4">
             {navLinks.map((link) => (
               <li
                 key={link.id}
                 className={`${
                   active === link.title ? "text-white" : " text-secondary"
-                } hover:text-white text-[18px] font-medium cursor-pointer`}
-                onClick={() => setActive(link.title)}
+                } font-poppins font-medium cursor-pointer text-[16px]`}
+                onClick={() => {
+                  setActive(link.title);
+                  setToggle(!toggle);
+                }}
               >
                 <a href={`#${link.id}`}>{link.title}</a>
               </li>

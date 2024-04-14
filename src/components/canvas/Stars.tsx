a7e18b5631a30a5ed9e6ab18881bfdfd4a66c09f
Gerson <gerson.toribio@pucp.pe>
2024-04-14 18:00:04 +0100

    add stars 3d component with animation

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,5 +1,49 @@
-const Stars = () => {
-  return <div>Stars</div>;
+import { useRef, Suspense, FC } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { Points as PointsType } from "three";
+
+import * as random from "maath/random/dist/maath-random.cjs";
+
+const Stars: FC = () => {
+  const ref = useRef<PointsType>(null);
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 }) as Float32Array;
+
+  useFrame((_state, delta) => {
+    if (ref && ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
-export default Stars;
+const StarsCanvas: FC = () => {
+  return (
+    <div className="2-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
